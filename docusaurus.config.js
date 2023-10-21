43349e0aa6e981d082ae65fbe2f0270a5e330046
Jose Rubicco Ferro <a23joserf@iessanclemente.net>
2023-10-21 03:42:53 +0200

    Changed configs to deploy to vercel

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://JoseRubicco21.github.io',
+  url: 'https://daw-1-prog.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/DAW1-PROG/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
