303684052d86e708dcdc6c5a06bf640176720d91
Jose Rubicco Ferro <a23joserf@iessanclemente.net>
2023-10-21 03:36:41 +0200

    Changed settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://JoseRubicco21.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/DAW1-PROG/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -44,6 +44,7 @@ const config = {
           routeBasePath: "/",
           remarkPlugins: [math],
           rehypePlugins: [katex],
+          editUrl:"https://github.com/JoseRubicco21/DAW1-PROG/tree/main/"
         },
         blog: false,
         theme: {
